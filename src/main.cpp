197410f51268f716ca23a723ea672b63b4bce30d
harksu <harksulim@naver.com>
2024-08-12 22:23:29 +0900

    chore: send_arp function

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,30 @@ Mac get_my_mac_address(const char* my_interface){
 	return Mac((uint8_t*)interface_structure.ifr_hwaddr.sa_data);
 }
 
+void send_Arp(pcap_t* handle, Mac source_mac, Ip source_ip, Mac target_mac, Ip target_ip ){
+	//기본적으로 스켈레톤 코드에서 작성된 arp 코드를 가져오되, 이번 과제에서는 단순히 하나의 타겟이 아님
+	EthArpPacket packet;
+
+	packet.eth_.dmac_ = Mac(target_mac);
+	packet.eth_.smac_ = Mac(source_mac);
+	packet.eth_.type_ = htons(EthHdr::Arp);
+
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+	packet.arp_.op_ = htons(ArpHdr::Request);
+	packet.arp_.smac_ = Mac(source_mac);
+	packet.arp_.sip_ = htonl(Ip(source_ip));
+	packet.arp_.tmac_ = Mac(target_mac);
+	packet.arp_.tip_ = htonl(Ip(target_ip));
+
+    int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+    if (res != 0) {
+        fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+    }
+}
+
 
 
 
@@ -68,26 +92,12 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	EthArpPacket packet;
+	
 
-	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.type_ = htons(EthHdr::Arp);
+	
 
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::SIZE;
-	packet.arp_.pln_ = Ip::SIZE;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
+	
 
-	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
-	}
 
 	pcap_close(handle);
 }
